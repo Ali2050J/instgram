db1fb38f8781c3bbcc622f6cdf80986006b5c218
Ali2050J <alijafarian138@gmail.com>
2023-10-06 20:34:06 +0330

    fix some buges in fornt

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    # 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3'
+    }
 }
 
 # Password validation
