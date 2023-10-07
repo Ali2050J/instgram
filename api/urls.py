50b8bc1893b978d4856cdf40971eca027e637b01
Ali2050J <alijafarian138@gmail.com>
2023-10-08 00:07:54 +0330

    split two api (favorite posts and posts)

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ app_name = 'api'
 
 urlpatterns = [
     path('posts/', view=views.PostListView.as_view(), name='post_list'),
+    path('favorite-posts/', view=views.FavoritePostListView.as_view(), name='favorite_post_list'),
 ]
--- api/views.py
+++ api/views.py
@@ -8,14 +8,20 @@ from .serializers import PostListSerializer, FavoritePostListSerializer
 class PostListView(APIView):
     def get(self, request):
         posts = Post.objects.all().order_by('id')
-        favorite_posts = Favorite.objects.all().order_by('id')
-        # serializers
         srz_data_posts = PostListSerializer(instance=posts, many=True)
-        srz_data_favorites = FavoritePostListSerializer(instance=favorite_posts, many=True)
-        for favorite_p in srz_data_favorites.data:
-            favorite_p['type'] = 'favorite'
 
         for post in srz_data_posts.data:
             post['type'] = 'post'
-        all = srz_data_posts.data + srz_data_favorites.data # Merge two data
-        return Response(data=all, status=status.HTTP_200_OK)
+
+        return Response(data=srz_data_posts.data, status=status.HTTP_200_OK)
+    
+
+class FavoritePostListView(APIView):
+    def get(self, request):
+        favorite_posts = Favorite.objects.all().order_by('id')
+        srz_data_favorites = FavoritePostListSerializer(instance=favorite_posts, many=True)
+
+        for favorite_p in srz_data_favorites.data:
+            favorite_p['type'] = 'favorite'
+
+        return Response(data=srz_data_favorites.data, status=status.HTTP_200_OK)
