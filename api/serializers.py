f9edf650e5da74f20728d13a1fe459108db470ea
Ali2050J <alijafarian138@gmail.com>
2023-10-07 18:05:51 +0330

    add favorites api and merge it with posts api

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from post.models import Post
+from post.models import Post, Favorite
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    type = 'post'
     class Meta:
         model = Post
+        fields = '__all__'
+
+
+class FavoritePostListSerializer(serializers.ModelSerializer):
+    type = 'favorite'
+
+    class Meta:
+        model = Favorite
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -2,11 +2,20 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
-from post.models import Post
-from .serializers import PostListSerializer
+from post.models import Post, Favorite
+from .serializers import PostListSerializer, FavoritePostListSerializer
 
 class PostListView(APIView):
     def get(self, request):
         posts = Post.objects.all().order_by('id')
-        srz_date = PostListSerializer(instance=posts, many=True)
-        return Response(data=srz_date.data, status=status.HTTP_200_OK)
+        favorite_posts = Favorite.objects.all().order_by('id')
+        # serializers
+        srz_data_posts = PostListSerializer(instance=posts, many=True)
+        srz_data_favorites = FavoritePostListSerializer(instance=favorite_posts, many=True)
+        for favorite_p in srz_data_favorites.data:
+            favorite_p['type'] = 'favorite'
+
+        for post in srz_data_posts.data:
+            post['type'] = 'post'
+        all = srz_data_posts.data + srz_data_favorites.data # Merge two data
+        return Response(data=all, status=status.HTTP_200_OK)
