0c8a481c0ecad2c826563a268e603488db2dcf79
Ali2050J <alijafarian138@gmail.com>
2023-10-10 13:39:51 +0330

    change urls

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('users/', view=views.UserListView.as_view(), name='user_list'),
     path('posts/', view=views.PostListView.as_view(), name='post_list'),
 
-    path('posts/<str:username>/', view=views.UserPostListView.as_view(), name='user_post_list'),
-    path('favorite-posts/<str:username>/', view=views.UserFavoritePostListView.as_view(), name='user_favorite_post_list'),
+    path('user_posts/<str:username>/', view=views.UserPostListView.as_view(), name='user_post_list'),
+    path('user_favorite-posts/<str:username>/', view=views.UserFavoritePostListView.as_view(), name='user_favorite_post_list'),
     path('post/likes/<int:post_id>/', view=views.PostLikeListView.as_view(), name='post_likes'),
-    path('followers/<str:username>/', view=views.UserFollowerListView.as_view(), name='user_follower_list'),
-    path('followings/<str:username>/', view=views.UserFollowingListView.as_view(), name='user_following_list'),
+    path('user_followers/<str:username>/', view=views.UserFollowerListView.as_view(), name='user_follower_list'),
+    path('user_followings/<str:username>/', view=views.UserFollowingListView.as_view(), name='user_following_list'),
 
     path('favorite/add/', view=views.AddFavoriteView.as_view(), name='add_favorite'),
     path('post/like/add/', view=views.AddLikeView.as_view(), name='add_like'),
