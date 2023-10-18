0d2ca7bb0d9dc239e5095189c6578fb88f772d4f
Ali2050J <alijafarian138@gmail.com>
2023-10-18 12:28:20 +0330

    optimization a home view

--- home/views.py
+++ home/views.py
@@ -6,10 +6,10 @@ from post.models import Post
 
 class HomeView(ListView):
     template_name = 'home/index.html'
-    queryset = Post.objects.filter(status='publish')
+    queryset = Post.objects.select_related('user').select_related('user__profile').prefetch_related('comments').filter(status='publish')
     context_object_name = 'posts'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.all()
+        context['users'] = User.objects.select_related('profile').all()
         return context
